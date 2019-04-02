a3d761b941042b11e7407e305dab4293e38622e5
cuican <cuican@fang.com>
2019-04-02 09:25:10 +0800

    测试点击事件

--- app/src/main/java/com/cc/skillapp/MainActivity.java
+++ app/src/main/java/com/cc/skillapp/MainActivity.java
@@ -2,12 +2,39 @@ package com.cc.skillapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvFuncitonOne;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        registerListener();
     }
+
+    private void registerListener() {
+        tvFuncitonOne.setOnClickListener(mOnClick);
+    }
+
+    private void initView() {
+        tvFuncitonOne = findViewById(R.id.tv_first_function);
+    }
+
+    View.OnClickListener mOnClick  = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.tv_first_function:
+                    Toast.makeText(MainActivity.this, "first function", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 }
